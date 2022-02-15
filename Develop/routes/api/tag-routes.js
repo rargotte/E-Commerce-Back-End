02036ba6a878f1c5925c0909ee7718f2600cb5e9
Regulo Argotte <rargotte@hotmail.com>
2022-02-15 07:36:51 -0600

    Version with working rout get all and get one for TAGS

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -16,10 +16,21 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+
+
 
 router.post('/', (req, res) => {
   // create a new tag
